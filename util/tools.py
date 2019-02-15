27a7ba41d05d2d2edf3156377692037663ef579f
ihyf <565267598@qq.com>
2019-02-15 19:25:16 +0800

    update

---
+++ util/tools.py
@@ -0,0 +1,6 @@
+from sanic_mako import SanicMako
+from sanic_auth import Auth
+
+
+mako = SanicMako()
+auth = Auth()
--- view/test.py
+++ view/test.py
@@ -1,11 +1,11 @@
 from sanic import Blueprint
+from util.tools import mako
 from sanic.response import json
 
 bp = Blueprint('test', url_prefix='/')
 
 
 @bp.route('/')
-async def bp_root(request):
-    b = bp
-    a = request
-    return json({'my': 'test'})
+@mako.template('index.html')
+async def index(request):
+    return {}
